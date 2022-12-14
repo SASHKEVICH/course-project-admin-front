9488ce1d5813bef5d25b845eb1c582cb23935b86
Aleksander Bekrenev <praise2001@yandex.ru>
2022-12-15 00:49:17 +0700

    feat: album routes

--- src/requests/genericRequest.js
+++ src/requests/genericRequest.js
@@ -1,7 +1,6 @@
 export const genRequest = async (request, configInit) => {
 	const response = await fetch(request, configInit);
 	const body = await response.json();
-	console.log(body);
 
 	if (response.status < 200 || response.status >= 300) {
 		throw Error(response.status);
